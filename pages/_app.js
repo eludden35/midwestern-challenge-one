5d46c83ba74b5b2cf1f5eabb5f4a8a1ea926b307
eludden35 <elonzo.ludden@gmail.com>
2022-02-21 21:33:22 -0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import logo from "../assets/Logo.png";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const clickHandler = (e) => {
+    e.preventDefault();
+    console.log(router);
+  };
+  return (
+    <div className="wrapper">
+      <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container-fluid ">
+          <a className="navbar-brand" href="#">
+            <Image src={logo} alt="midwestern-logo" />
+          </a>
+
+          <span className="navbar-text gold">
+            {router.pathname == "/contact" ? (
+              <Link href="/">home</Link>
+            ) : (
+              <Link href="/contact">contact</Link>
+            )}
+          </span>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
