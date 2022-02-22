2e67953dfd14fde7e1c6c4142d7c4d9fccbd938f
eludden35 <elonzo.ludden@gmail.com>
2022-02-21 22:14:53 -0600

    fix spacing and img alt

--- pages/contact.js
+++ pages/contact.js
@@ -2,100 +2,114 @@ import Head from "next/head";
 import styles from "../styles/Contact.module.css";
 
 export default function Contact() {
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-  };
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+    };
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>MIDWESTERN - Contact</title>
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>MIDWESTERN - Contact</title>
+            </Head>
 
-      <main className="h-100 row contact">
-        <div className="col-md-6 d-flex align-items-center">
-          <div className="row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8 text-white contact-words">
-              <h1 className="mb-4">
-                <span className={styles.h1UnderLine}>Heading</span> One
-              </h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                dos eiusmod tempor incididunt ut labore et trace dolore magna
-                aliqua.
-              </p>
-              <p>
-                Proin sagittis nisl rhoncus mattic rhoncus. At augue eget arcu
-                dictum varius duis at consectetur lorem.
-              </p>
-            </div>
-            <div className="col-md-2"></div>
-          </div>
+            <main className="h-100 row contact">
+                <div className="col-md-6 d-flex align-items-center">
+                    <div className="row">
+                        <div className="col-md-2"></div>
+                        <div className="col-md-8 text-white contact-words">
+                            <h1 className="mb-4">
+                                <span className={styles.h1UnderLine}>
+                                    Heading
+                                </span>{" "}
+                                One
+                            </h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do dos eiusmod tempor
+                                incididunt ut labore et trace dolore magna
+                                aliqua.
+                            </p>
+                            <p>
+                                Proin sagittis nisl rhoncus mattic rhoncus. At
+                                augue eget arcu dictum varius duis at
+                                consectetur lorem.
+                            </p>
+                        </div>
+                        <div className="col-md-2"></div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 d-flex align-items-center bg-white contactForm">
+                    <div className="row w-100">
+                        <div className="col-md-2"></div>
+                        <div className="col-md-8">
+                            <h2 className="mb-4">Heading Two</h2>
+                            <form onSubmit={(e) => onSubmitHandler(e)}>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <input
+                                            className={styles.inputs}
+                                            type="text"
+                                            placeholder="First Name"
+                                            onInvalid={(e) =>
+                                                e.target.setCustomValidity(
+                                                    "Required"
+                                                )
+                                            }
+                                            onInput={(e) =>
+                                                e.target.setCustomValidity("")
+                                            }
+                                        />
+                                    </div>
+                                    <div className="col-md-6">
+                                        <input
+                                            className={styles.inputs}
+                                            type="text"
+                                            placeholder="Last Name"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <input
+                                            className={styles.inputs}
+                                            type="text"
+                                            placeholder="Title"
+                                        />
+                                    </div>
+                                    <div className="col-md-6">
+                                        <input
+                                            className={styles.inputs}
+                                            type="email"
+                                            placeholder="Email"
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <textarea
+                                            className={styles.textArea}
+                                            placeholder="Message"
+                                        ></textarea>
+                                    </div>
+                                </div>
+                                <div className="row d-flex justify-content-center">
+                                    <div className="col-md-3 d-flex justify-content-center mt-2">
+                                        <button
+                                            className="btn rounded-0 text-white"
+                                            type="submit"
+                                        >
+                                            Submit
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="col-md-2"></div>
+                    </div>
+                </div>
+            </main>
         </div>
-
-        <div className="col-md-6 d-flex align-items-center bg-white contactForm">
-          <div className="row w-100">
-            <div className="col-md-2"></div>
-            <div className="col-md-8">
-              <h2 className="mb-4">Heading Two</h2>
-              <form onSubmit={(e) => onSubmitHandler(e)}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <input
-                      className={styles.inputs}
-                      type="text"
-                      placeholder="First Name"
-                      onInvalid={(e) => e.target.setCustomValidity("Required")}
-                      onInput={(e) => e.target.setCustomValidity("")}
-                    />
-                  </div>
-                  <div className="col-md-6">
-                    <input
-                      className={styles.inputs}
-                      type="text"
-                      placeholder="Last Name"
-                    />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-6">
-                    <input
-                      className={styles.inputs}
-                      type="text"
-                      placeholder="Title"
-                    />
-                  </div>
-                  <div className="col-md-6">
-                    <input
-                      className={styles.inputs}
-                      type="email"
-                      placeholder="Email"
-                      required
-                    />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-12">
-                    <textarea
-                      className={styles.textArea}
-                      placeholder="Message"
-                    ></textarea>
-                  </div>
-                </div>
-                <div className="row d-flex justify-content-center">
-                  <div className="col-md-3 d-flex justify-content-center mt-2">
-                    <button className="btn rounded-0 text-white" type="submit">
-                      Submit
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div className="col-md-2"></div>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,105 +9,103 @@ import toast from "react-simple-toasts";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  let nameObjects = [
-    {
-      one: "Matt Johnson",
-      two: "Bart Paden",
-      three: "Ryan Doss",
-      four: "Jared Malcolm",
-    },
-    {
-      one: "Matt Johnson",
-      two: "Bart Paden",
-      three: "Jordan Heigle",
-      four: "Tyler Viles",
-    },
-  ];
+    let nameObjects = [
+        {
+            one: "Matt Johnson",
+            two: "Bart Paden",
+            three: "Ryan Doss",
+            four: "Jared Malcolm",
+        },
+        {
+            one: "Matt Johnson",
+            two: "Bart Paden",
+            three: "Jordan Heigle",
+            four: "Tyler Viles",
+        },
+    ];
 
-  const [puzzleRes, setPuzzleRes] = useState([]);
-  const showPuzzleResults = () => {
-    let distinctNames = [];
-    Object.keys(nameObjects[0]).forEach((key) => {
-      if (nameObjects[0][key] == nameObjects[1][key]) {
-        distinctNames.push(nameObjects[0][key]);
-      }
+    const [puzzleRes, setPuzzleRes] = useState([]);
+    const showPuzzleResults = () => {
+        let distinctNames = [];
+        Object.keys(nameObjects[0]).forEach((key) => {
+            if (nameObjects[0][key] == nameObjects[1][key]) {
+                distinctNames.push(nameObjects[0][key]);
+            }
 
-      if (nameObjects[0][key] != nameObjects[1][key]) {
-        distinctNames.push(nameObjects[0][key]);
-        distinctNames.push(nameObjects[1][key]);
-      }
-    });
+            if (nameObjects[0][key] != nameObjects[1][key]) {
+                distinctNames.push(nameObjects[0][key]);
+                distinctNames.push(nameObjects[1][key]);
+            }
+        });
 
-    puzzleRes.length > 0
-      ? toast("Results are already displayed!")
-      : setPuzzleRes(distinctNames);
-  };
+        puzzleRes.length > 0
+            ? toast("Results are already displayed!")
+            : setPuzzleRes(distinctNames);
+    };
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>MIDWESTERN - Home</title>
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>MIDWESTERN - Home</title>
+            </Head>
 
-      <main className={styles.mainContainer}>
-        <div className={styles.infoBox}>
-          <div className={styles.boxImg}>
-            <Image src={talkie} alt="walkie-talkie" />
-          </div>
-          <h2>Heading Two</h2>
-          <p>
-            Integer accumsan moleste nisl, id faucibus urna accumsan quis. Proin
-            vulputate, mauris semper maximus.
-          </p>
-          <button className="btn rounded-0">Learn more</button>
+            <main className={styles.mainContainer}>
+                <div className={styles.infoBox}>
+                    <div className={styles.boxImg}>
+                        <Image src={talkie} alt="walkie-talkie" />
+                    </div>
+                    <h2>Heading Two</h2>
+                    <p>
+                        Integer accumsan moleste nisl, id faucibus urna accumsan
+                        quis. Proin vulputate, mauris semper maximus.
+                    </p>
+                    <button className="btn rounded-0">Learn more</button>
+                </div>
+
+                <div className={styles.infoBox}>
+                    <div className={styles.boxImg}>
+                        <Image src={rabbit} alt="rabbit" />
+                    </div>
+                    <h2>Heading Two</h2>
+                    <p>Lo</p>
+                    <button className="btn rounded-0">Learn more</button>
+                </div>
+
+                <div className={styles.infoBox}>
+                    <div className={styles.boxImg}>
+                        <Image src={shield} alt="shield" />
+                    </div>
+                    <h2>Heading Two</h2>
+                    <p>
+                        Integer accumsan moleste nisl, id faucibus urna accumsan
+                        quis. Proin vulputate, mauris semper maximus.
+                    </p>
+                    <button className="btn rounded-0">Learn more</button>
+                </div>
+            </main>
+
+            <footer className={styles.footerRow}>
+                <h1>
+                    <span className={styles.h1UnderLine}>Heading</span> One
+                </h1>
+                <p>
+                    Remove the duplicate in 2 javascript objects (found in
+                    readme), add the results to an array and output the list of
+                    distinct names in an unordered list below this paragraph
+                    when{" "}
+                    <Link href="" className={styles.link}>
+                        <a onClick={() => showPuzzleResults()}>this link</a>
+                    </Link>{" "}
+                    is clicked. If the operation has been completed already,
+                    notify the user that this has already been done.
+                </p>
+
+                <ul>
+                    {puzzleRes.length > 0
+                        ? puzzleRes.map((name, i) => <li key={i}>{name}</li>)
+                        : ""}
+                </ul>
+            </footer>
         </div>
-
-        <div className={styles.infoBox}>
-          <div className={styles.boxImg}>
-            <Image src={rabbit} alt="walkie-talkie" />
-          </div>
-          <h2>Heading Two</h2>
-          <p>
-            Integer accumsan moleste nisl, id faucibus urna accumsan quis. Proin
-            vulputate, mauris semper maximus.
-          </p>
-          <button className="btn rounded-0">Learn more</button>
-        </div>
-
-        <div className={styles.infoBox}>
-          <div className={styles.boxImg}>
-            <Image src={shield} alt="walkie-talkie" />
-          </div>
-          <h2>Heading Two</h2>
-          <p>
-            Integer accumsan moleste nisl, id faucibus urna accumsan quis. Proin
-            vulputate, mauris semper maximus.
-          </p>
-          <button className="btn rounded-0">Learn more</button>
-        </div>
-      </main>
-
-      <footer className={styles.footerRow}>
-        <h1>
-          <span className={styles.h1UnderLine}>Heading</span> One
-        </h1>
-        <p>
-          Remove the duplicate in 2 javascript objects (found in readme), add
-          the results to an array and output the list of distinct names in an
-          unordered list below this paragraph when{" "}
-          <Link href="" className={styles.link}>
-            <a onClick={() => showPuzzleResults()}>this link</a>
-          </Link>{" "}
-          is clicked. If the operation has been completed already, notify the
-          user that this has already been done.
-        </p>
-
-        <ul>
-          {puzzleRes.length > 0
-            ? puzzleRes.map((name, i) => <li key={i}>{name}</li>)
-            : ""}
-        </ul>
-      </footer>
-    </div>
-  );
+    );
 }
